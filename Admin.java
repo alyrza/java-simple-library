2d0048bff735e5d6d872c61263cf7094e51b2dac
alyrza <alyrzanm@gmail.com>
2023-05-01 21:35:07 +0430

    complete admin class

---
+++ Admin.java
@@ -0,0 +1,26 @@
+public class Admin {
+    private String name;
+    private String email;
+
+    public Admin(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and setters for the private fields
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
