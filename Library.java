c72a4ec1345ee510587474fc822015b6c248a362
alyrza <alyrzanm@gmail.com>
2023-05-01 21:50:41 +0430

    complete library class

--- Library.java
+++ Library.java
@@ -1,6 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
+    private List<Book> books;
+    private List<User> users;
+    private List<Admin> admins;
+
+    public Library() {
+        this.books = new ArrayList<>();
+        this.users = new ArrayList<>();
+        this.admins = new ArrayList<>();
     }
 
+    // Getters for the private fields
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Admin> getAdmins() {
+        return admins;
+    }
+
+    // Methods for adding and removing books, users and admins
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        this.books.remove(book);
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    public void removeUser(User user) {
+        this.users.remove(user);
+    }
+
+    public void addAdmin(Admin admin) {
+        this.admins.add(admin);
+    }
+
+    public void removeAdmin(Admin admin) {
+        this.admins.remove(admin);
+    }
 }
