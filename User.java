32ccaf451353b852808aaa62c41a270bd67aa69f
alyrza <alyrzanm@gmail.com>
2023-05-01 21:28:16 +0430

    complete user class

---
+++ User.java
@@ -0,0 +1,26 @@
+public class User {
+    private String name;
+    private int studentId;
+
+    public User(String name, int studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    // Getters and setters for the private fields
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
